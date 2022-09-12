fcd2c86a933897d5a06f6655f71a1dbd04d85598
emilianovaccaro <91967480+emilianovaccaro@users.noreply.github.com>
2022-09-12 16:13:08 -0300

    fix route for deploy (#25)

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 // FRONTEND BUILD
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
   app.get('*', (req, res) => res.sendFile(
     path.resolve(__dirname, '../', 'client', 'dist', 'index.html')
