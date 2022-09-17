21a9aedcd982808fe7ea53d7ea92fc13845eeb1a
emilianovaccaro <91967480+emilianovaccaro@users.noreply.github.com>
2022-09-17 15:06:07 -0300

    Redux store added (#32)
    * redux store, both auth and tasks, also minor changes in backend
    
    * omit payload property on user actions
    
    * fix route in redux actions
    
    * fix indent spaces
    
    * added options on eslint

---
+++ client/src/redux/actions/tasksActions.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { 
+  FETCH_TASKS,
+  CREATE_TASK,
+  UPDATE_TASK,
+  DELETE_TASK } from './types';
+
+
+const API_URL = 'https://test-server-tasky.herokuapp.com';
+
+
+//get
+export const fetchTasks = ( token ) => async ( dispatch ) => {
+  const res = await axios.get(`${API_URL}/api/task`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  });
+
+  dispatch({ type: FETCH_TASKS, payload: res.data });
+};
+
+//create
+export const createTask = ( values, token ) => async ( dispatch ) => {
+  const res = await axios.post(`${API_URL}/api/task`, values, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  dispatch({ type: CREATE_TASK, payload: res.data });
+};
+
+//delete
+export const deleteTask = ( id, token ) => async ( dispatch ) => {
+  await axios.delete(`${API_URL}/api/task/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  dispatch({ type: DELETE_TASK, payload: id });
+};
+
+//update
+export const updateTask = ( id, values, token ) => async ( dispatch ) => {
+  const res = await axios.patch(`${API_URL}/api/task/${id}`, values, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  dispatch({ type: UPDATE_TASK, payload: res.data });
+};
