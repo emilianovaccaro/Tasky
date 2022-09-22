d9347a563db268682b01a987fe55d60a4ece6c5a
unknown <evaccarocosta.1@gmail.com>
2022-09-22 00:46:46 -0300

    fix

--- client/src/helpers/ProtectSidebar.jsx
+++ client/src/helpers/ProtectSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Sidebar } from '../components/sidebar'
+import { Sidebar } from '../components/Sidebar'
 import { useSelector } from 'react-redux'
 
 const ProtectSidebar = () => {
