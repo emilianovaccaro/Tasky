73894384dce74412aae56e037eff4bb9f735396a
emilianovaccaro <91967480+emilianovaccaro@users.noreply.github.com>
2022-09-24 14:11:42 -0300

    fix validations, fix token middleware (#50)

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,29 +1,40 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
+
 const protect = async (req, res, next) => {
   //let JWT
   let token
+
   // CHECK AUTHORIZATION HEADERS, 'Bearer jwtoken'
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
 
       token = req.headers.authorization.split(' ')[1]
+
+      if (!token) {
+        return res.status(500).json({msg: 'USER UNAUTHORIZED - No token'})
+      }
+      
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
       //Get user by token
       //Exclude password
       req.user = await User.findById(decoded.id).select('-password -teamPassword')
+
+      if(!req.user){
+        return res.status(500).json({msg: 'USER UNAUTHORIZED - No token'})
+      }
       
       next()
 
     } catch (error) {
-      res.status(500).json(error)
+      return res.status(500).json(error)
     }
   }
 
   if (!token) {
-    res.status(401).json({msg: 'you must log in first'})
+    return res.status(500).json({msg: 'USER UNAUTHORIZED - No token'})
   }
 }
 
