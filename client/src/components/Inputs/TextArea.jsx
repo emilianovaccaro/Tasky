d2daec34cba60523196cb3d002ae4b9ef47b19c0
unknown <evaccarocosta.1@gmail.com>
2022-09-26 00:50:35 -0300

    task comment form added

--- client/src/components/Inputs/TextArea.jsx
+++ client/src/components/Inputs/TextArea.jsx
@@ -3,16 +3,17 @@ import styled from 'styled-components'
 import { Label } from '../Text/Label'
 import { SubLabel } from '../Text/SubLabel'
 
-export const TextArea = ( {inputLabel, name, onChange, value, id, error, maxLength} ) => {
+export const TextArea = ( {inputLabel, name, onChange, value, id, error, maxLength, height} ) => {
   return(
     <>
       <Label marginBottom={'16px'} htmlFor={id}>{inputLabel}</Label>
-      {error && <SubLabel error>{error}</SubLabel>}
+      {error && <SubLabel errorTextArea error>{error}</SubLabel>}
       <InputTextArea
         id={id}
         name={name}
         onChange={onChange}
         value={value}
+        height={height}
         error={error}
         maxLength={maxLength}
       />
@@ -25,6 +26,7 @@ const InputTextArea = styled.textarea`
   resize: none;
   display: block;
   width: 100%;
+  height: ${p => p.height && '128px'};
   box-sizing: border-box;
   padding: ${p => p.theme.styles.padding.small};
   font-weight: ${p => p.theme.styles.fontWeight.medium};
